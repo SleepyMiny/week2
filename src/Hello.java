c270d705b273a3a97340762b4fbcb3edac6bae32
Sleepyminy <dark47297@naver.com>
2024-09-18 16:26:01 +0900

    11

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 3.14;
+        boolean c = true;
+        char d = 't';
+        String e = "st";
+
+        System.out.println(a);
+
+    }
+}
